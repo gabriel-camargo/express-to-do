e9410fa11b5dee0f7ca54007faf30ef2a50928d0
Gabriel R. Camargo <gabriel.rodrigocam@gmail.com>
2021-04-13 23:28:33 -0300

    eslint and prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import server from "./server"
+import server from './server'
 
 server.app.listen(server.port, () => {
-  console.log(`Running in ${server.mode} mode on port ${server.port}`)
+    console.log(`Running in ${server.mode} mode on port ${server.port}`)
 })
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express, { Application, json } from "express";
-import cors from "cors";
-import helmet from "helmet";
-import { config } from "dotenv";
+import express, { Application, json } from 'express'
+import cors from 'cors'
+import helmet from 'helmet'
+import { config } from 'dotenv'
 
 class App {
-
     public app: Application
     public port: string | number
     public mode: string
@@ -15,18 +14,18 @@ class App {
         config()
 
         this.app = express()
-        this.port = process.env.PORT || 3000;
+        this.port = process.env.PORT || 3000
         this.mode = process.env.NODE_ENV ?? 'development'
 
-        this.isDev =this.mode !== `production`
+        this.isDev = this.mode !== `production`
 
         this.middlewares()
     }
 
     private middlewares(): void {
-      this.app.use(helmet());
-      this.app.use(cors());
-      this.app.use(json());
+        this.app.use(helmet())
+        this.app.use(cors())
+        this.app.use(json())
     }
 }
 
